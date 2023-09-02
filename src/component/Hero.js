7d5bac00dd4d0b034a4d17b4b43e065892fc514f
srmahour <sadeepmahour@gmail.com>
2023-09-02 13:45:26 +0530

    changes in hero title

--- src/component/Hero.js
+++ src/component/Hero.js
@@ -12,8 +12,8 @@ export default function Hero(){
             <div className="max-w-8xl container relative mx-auto">
                 <div className="flex flex-wrap items-center">
                     <div className="ml-auto mr-auto w-full px-4 text-center lg:w-8/12">
-                        <h1 className="block antialiased tracking-normal font-sans text-5xl leading-tight text-white mb-6 font-black">Your story starts with us.</h1>
-                        <p className="block antialiased font-sans text-xl font-normal leading-relaxed text-white opacity-80">This is a simple example of a Landing Page you can build using Material Tailwind. It features multiple components based on the Tailwind CSS and Material Design by Google.</p>
+                        <h1 className="block antialiased tracking-normal font-sans text-5xl leading-tight text-white mb-6 font-black">E-SHOP</h1>
+                        <p className="block antialiased font-sans text-xl font-normal leading-relaxed text-white opacity-80">Small E-shop with the help of React, Fake product API and Redux-toolkit</p>
                     </div>
                 </div>
             </div>
